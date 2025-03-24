f4c82bf3be2929d5c03dc50f9c55360aa01ad052
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-03-24 18:14:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	var fname = document.getElementById("fanme").value;
+	var fname = document.getElementById("lanme").value;
+	alert(fname + " " + lname);
+	
 }
